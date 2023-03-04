03aa8976fa0edb10c43a0b7110ee61bf82367d4a
schktjm <schktjm@gmail.com>
2023-03-04 15:16:25 +0900

    :+1: analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
@@ -18,19 +19,25 @@ const getPublicFileList = async (targetPath: string) => {
   return publicFiles;
 };
 
-export default defineConfig(async () => {
+export default defineConfig(async ({mode}) => {
   const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
 
   return {
     build: {
-      assetsInlineLimit: 20480,
-      cssCodeSplit: false,
-      cssTarget: 'es6',
+      assetsInlineLimit: 2048,
       minify: false,
       rollupOptions: {
         output: {
-          experimentalMinChunkSize: 40960,
+          experimentalMinChunkSize: 4096,
         },
+        plugins: [
+          mode === 'analyze' &&  visualizer({
+            brotliSize: true,
+            filename: 'dist/stats.html',
+            gzipSize: true,
+            open: true,
+          }),
+        ],
       },
       target: 'es2015',
     },
